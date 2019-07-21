9ccdb5e399e6551c9375e88844add12bf49a2d8b
GEscor <escor@inbox.ru>
2019-07-21 18:06:05 +0700

    Updates

---
+++ precache-manifest.cdfd1ca4f88bb5944c3b26149ce35021.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9be8ae764aaa304dfb73113f9aa9e56c",
+    "url": "/ToDo-test/index.html"
+  },
+  {
+    "revision": "81c31a0d7002223b31cd",
+    "url": "/ToDo-test/static/css/main.7a971fbd.chunk.css"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/ToDo-test/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "81c31a0d7002223b31cd",
+    "url": "/ToDo-test/static/js/main.85bc64c6.chunk.js"
+  },
+  {
+    "revision": "31b6dee6e633201c84af",
+    "url": "/ToDo-test/static/js/runtime~main.431b8c02.js"
+  }
+]);
